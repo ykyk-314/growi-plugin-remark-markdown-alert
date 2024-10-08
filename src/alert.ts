b8b96d479d5551baeaa4d6df3506007d3582ee2c
yokoyan <39142831+ykyk-314@users.noreply.github.com>
2024-10-08 13:44:13 +0900

    updated

--- src/alert.ts
+++ src/alert.ts
@@ -21,23 +21,37 @@ export const plugin: Plugin = function() {
           // paragraph内の最初のテキスト要素
           const textNode = paragraph.children[0];
           if (textNode.type === 'text' && textNode.value) {
-            // テキストを取得してtrim()を適用
             const content = textNode.value.trim();
-            console.log(content);
+            const match = content.match(/^\[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\](.*)$/);
 
-            const match = content
-              .match(/^\[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\](.*)$/);
+            console.log(content);
+            console.log(match);
             if (match) {
-              console.log(match);
               const alertType = match[1].toLowerCase(); // NOTE, TIP, WARNING など
-              const alertContent = match[2].trim(); // アラート内容
+
+              // アラート記法のタグ部分をスキップし、それ以外の内容を全て取得
+              const remainingContent = paragraph.children.slice(1);
+
+              // 残りのコンテンツを連結してHTMLとして出力する
+              const innerHTML = remainingContent.map((child) => {
+                if (child.type === 'text') {
+                  return `<p>${child.value}</p>`;
+                }
+                if (child.type === 'break') {
+                  return '<br>';
+                }
+                if (child.type === 'paragraph') {
+                  return `<p>${child.children.map(subChild => subChild.value || '').join('')}</p>`;
+                }
+                return '';
+              }).join('');
+              console.log(JSON.parse(JSON.stringify(innerHTML)));
 
               // HTMLとしてカスタムアラートを生成
               node.type = 'html';
               node.value = `
                 <div class="callout callout-${alertType}">
-                  <strong>${alertType.toUpperCase()}</strong>
-                  <p>${alertContent}</p>
+                  <blockquote>${innerHTML}</blockquote>
                 </div>
               `;
               // 子要素をクリア
